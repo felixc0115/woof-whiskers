111ae9a725a3544e1763af3a99555dac8c716abf
Felix Chen <fechen1993@gmail.com>
2023-09-27 11:22:51 -0400

    add unit tests for creating a user and super user

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from .models import CustomUser
+# instead of importing CustomUser from .models, I used the get_user_model function which will reference the AUTH_USER_MODEL in the settings.py file which is what we use to set the user
+from django.contrib.auth import get_user_model
 
 
+# although excluding class Meta would work, including it has more benefits to isolate the metadata of the class also to prevent name clashing.
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = UserCreationForm.Meta.fields + ("name",)
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = UserChangeForm.Meta.fields
