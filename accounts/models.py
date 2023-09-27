f963205571d6ab57d31b509e7aaa96a7a7b2d8c1
Felix Chen <fechen1993@gmail.com>
2023-09-26 23:01:02 -0400

    add accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+# I chose to create a CustomUser model that is a subclass of AbstractUser because it allows for easier customization than using the built-in User model
+class CustomUser(AbstractUser):
+    name = models.CharField(null=True, blank=True, max_length=100)
