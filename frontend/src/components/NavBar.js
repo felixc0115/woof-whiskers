9f3c6e6f236943aaf3a8ef318555316cabbf5890
Felix Chen <fechen1993@gmail.com>
2023-10-01 22:44:33 -0400

    fix login and signup nav buttons

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,6 +9,7 @@ import {
   Tooltip,
   Typography,
   Toolbar,
+  Button,
 } from "@mui/material";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
@@ -45,65 +46,75 @@ const NavBar = () => {
           Woof & Whiskers
         </Typography>
         <Pets sx={{ display: { xs: "block", sm: "none" } }} />
-
-        <Box sx={{ flexGrow: 0 }}>
-          <Typography
-            sx={{ mr: "8px" }}
-            variant="span"
-          >{` welcome, ${user?.username}!`}</Typography>
-          <Tooltip title="Open settings">
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar
-                alt="profile"
-                src="https://images.pexels.com/photos/3402575/pexels-photo-3402575.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: "45px" }}
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            <MenuItem>
-              <Link
-                onClick={handleCloseUserMenu}
-                style={{ color: "black", textDecoration: "none" }}
-                to="/"
-              >
-                Home
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link
-                onClick={handleCloseUserMenu}
-                style={{ color: "black", textDecoration: "none" }}
-                to="/pets"
-              >
-                Explore
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link
-                onClick={handleCloseUserMenu}
-                style={{ color: "black", textDecoration: "none" }}
-                to="/favorites"
-              >
-                Favorites
-              </Link>
-            </MenuItem>
-            <MenuItem onClick={logoutHandler}>Logout</MenuItem>
-          </Menu>
-        </Box>
+        {!user ? (
+          <Box>
+            <Button variant="container" onClick={() => navigate("/login")}>
+              Login
+            </Button>
+            <Button variant="container" onClick={() => navigate("/signup")}>
+              Signup
+            </Button>
+          </Box>
+        ) : (
+          <Box sx={{ flexGrow: 0 }}>
+            <Typography
+              sx={{ mr: "8px" }}
+              variant="span"
+            >{` welcome, ${user?.username}!`}</Typography>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="profile"
+                  src="https://images.pexels.com/photos/3402575/pexels-photo-3402575.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem>
+                <Link
+                  onClick={handleCloseUserMenu}
+                  style={{ color: "black", textDecoration: "none" }}
+                  to="/"
+                >
+                  Home
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link
+                  onClick={handleCloseUserMenu}
+                  style={{ color: "black", textDecoration: "none" }}
+                  to="/pets"
+                >
+                  Explore
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link
+                  onClick={handleCloseUserMenu}
+                  style={{ color: "black", textDecoration: "none" }}
+                  to="/favorites"
+                >
+                  Favorites
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </Menu>
+          </Box>
+        )}
       </Toolbar>
     </AppBar>
   );
